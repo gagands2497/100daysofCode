3ab4578133938ac03132bc61b9c01767aa6ecbb5
gagands2497 <gagands2497@gmail.com>
2021-10-01 12:04:39 +0530

    day1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 100daysofCode
+# Will start chapter wise from the book 
+
+Antti Laaksonen
+Guide to Competitive Programming
+Learning and Improving Algorithms Through Contests
---
+++ day1/c++template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+
+void FIO()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+}
+
+
+
+int main()
+{
+    FIO();
+    int i;
+    REP(i,0,10){
+        cout << i << "";
+    }
+}
